a8fccacee57605c2998cc0cccf18a8a558421972
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2019-09-29 16:14:22 +0900

    りんごの税込価格を表示する機能を追加

--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { stat } from 'fs'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    apple: 150
+  },
+  getters: {
+    //税込み金額
+    appleIncludesTax: state => {
+      return state.apple * 1.08
+    }
   },
   mutations: {
 
